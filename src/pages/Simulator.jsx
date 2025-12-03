f5fcec8cdd9b0ab1a84da0c43ad94dab01145634
oquendo2222 <josedavidoquendohenao@gmail.com>
2025-12-02 22:03:12 -0500

    Cálculo de cuota mensual funcionando

--- src/pages/Simulator.jsx
+++ src/pages/Simulator.jsx
@@ -3,10 +3,25 @@ import React from 'react'
 export const Simulator = () => {
   return (
     <div>
-      Simulator
+      
     </div>
   )
 }
 
+/*const calcularCuota = (monto, tasaInteres, meses) => {
+  const i = tasaInteres / 100 / 12; // convertir % a interés mensual
+  const n = meses;
 
+  if (i === 0) return monto / n; // caso sin interés
 
+  const cuota =
+    monto *
+    (i * Math.pow(1 + i, n)) /
+    (Math.pow(1 + i, n) - 1);
+
+  return cuota.toFixed(2);
+};const handleCalcular = () => {
+  const cuota = calcularCuota(monto, interes, plazo);
+  setCuotaMensual(cuota);
+};
+*/
